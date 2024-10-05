9fbd6e3c61c44e41838243600db85640b87aadf2
Chernousov Anton <chernousov@interlan.xyz>
2024-10-05 08:24:46 +0700

    lesson-1

---
+++ python/lesson-1/1-lesson.py
@@ -0,0 +1,21 @@
+#/usr/bin/python3
+
+# 1st program
+print ((9**0.5)*5)
+
+# 2nd program
+print ((9.99>9.98) and (1000!=1000.1))
+
+# 3rd program
+print (2*2+2)
+print ((2*(2+2)))
+print ((2*2+2)==(2*(2+2)))
+
+# 4th program
+a = 123.456
+b = str(a)
+print (int(b.split(".")[1][0]))
+c= int(b.split(".")[1][0])
+d= float (b.split(".")[0]+str(c)+'.'+b.split(".")[1])
+print (d)
+
